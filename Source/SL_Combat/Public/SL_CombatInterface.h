49cc92598da163b98c5ac490102ab578aec06c15
ahmedferoz196 <154981864+ahmedferoz196@users.noreply.github.com>
2026-05-23 16:59:38 +0500

    Combat Interface

---
+++ Source/SL_Combat/Public/SL_CombatInterface.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SL_CombatInterface.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SL_COMBAT_API USL_CombatInterface : public UObject
+{
+	GENERATED_BODY()
+	
+};
